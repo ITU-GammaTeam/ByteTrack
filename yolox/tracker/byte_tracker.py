4629718457a9e9f8bf190eb3fb6d56c2137a1858
goktug311 <gamma.itu@gmail.com>
2025-08-16 18:18:49 +0300

    goktug duzeltme

--- yolox/tracker/byte_tracker.py
+++ yolox/tracker/byte_tracker.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn.functional as F
 
 from .kalman_filter import KalmanFilter
-from yolox.tracker import matching
+from cv_process.lib.ByteTrack.yolox.tracker import matching
 from .basetrack import BaseTrack, TrackState
 
 class STrack(BaseTrack):
